cc8ef951d5f58344cee2ddba25dccdaabf1d7e2a
Sayemeen Islam <173556073+sayemeen-islam@users.noreply.github.com>
2026-09-21 12:37:51 +0600

    fix: handle data fetching errors

--- src/components/homepage/Books.tsx
+++ src/components/homepage/Books.tsx
@@ -3,9 +3,15 @@ import BookCard from "../shared/BookCard";
 import { IBook } from "@/types/books-type";
 
 const getBook = async () => {
-  const res = await fetch("http://localhost:3000/booksData.json");
-  const data = await res.json();
-  return data;
+  try{
+      const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`);
+      const data = await res.json();
+      return data;
+
+  }catch(error){
+    console.error("Error fetching books data:", error);
+    return [];
+  }
 };
 
 const Books = async () => {
